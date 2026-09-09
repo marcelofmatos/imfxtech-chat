b3bba95509dc7633b3a4f0484c755f0c24b6af81
Marcelo Matos <contato@marcelomatos.dev>
2026-09-09 14:42:17 -0400

    feat: rebrand app_config.dart identifiers, theme color and links to IMFxTech Chat

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -6,7 +6,7 @@
 import 'dart:ui';
 
 abstract class AppConfig {
-  static const Color primaryColor = Color(0xFF261386);
+  static const Color primaryColor = Color(0xFFD8A954);
 
   static const Color chatColor = primaryColor;
   static const double messageFontSize = 16.0;
@@ -15,10 +15,10 @@ abstract class AppConfig {
   static const bool hideTypingUsernames = false;
 
   static const String inviteLinkPrefix = 'https://matrix.to/#/';
-  static const String deepLinkPrefix = 'im.fluffychat://chat/';
+  static const String deepLinkPrefix = 'com.imfxtech.chat://chat/';
   static const String schemePrefix = 'matrix:';
-  static const String pushNotificationsChannelId = 'fluffychat_push';
-  static const String pushNotificationsAppId = 'chat.fluffy.fluffychat';
+  static const String pushNotificationsChannelId = 'imfxtech_chat_push';
+  static const String pushNotificationsAppId = 'com.imfxtech.chat';
   static const double borderRadius = 18.0;
   static const double spaceBorderRadius = 11.0;
   static const double columnWidth = 360.0;
@@ -29,30 +29,30 @@ abstract class AppConfig {
       'https://ko-fi.com/post/How-to-use-end-to-end-encryption-in-FluffyChat-A5O725WDR5';
   static const String howDoIGetStickersTutorial =
       'https://ko-fi.com/post/How-to-add-a-sticker-pack-to-FluffyChat-N4N01OXATI';
-  static const String appId = 'im.fluffychat.FluffyChat';
-  static const String appOpenUrlScheme = 'im.fluffychat';
-  static const String appSsoUrlScheme = 'im.fluffychat.auth';
+  static const String appId = 'com.imfxtech.chat.IMFxTechChat';
+  static const String appOpenUrlScheme = 'com.imfxtech.chat';
+  static const String appSsoUrlScheme = 'com.imfxtech.chat.auth';
 
   static const String sourceCodeUrl =
-      'https://github.com/krille-chan/fluffychat';
+      'https://github.com/marcelofmatos/imfxtech-chat';
   static const String supportUrl =
-      'https://github.com/krille-chan/fluffychat/issues';
-  static const String changelogUrl = 'https://fluffychat.im/changelog/';
-  static const String helpUrl =
-      'https://ko-fi.com/post/How-can-I-support-FluffyChat-J2G325WE6I';
+      'https://github.com/marcelofmatos/imfxtech-chat/issues';
+  static const String changelogUrl =
+      'https://github.com/marcelofmatos/imfxtech-chat/blob/main/CHANGELOG.md';
 
   static const Set<String> defaultReactions = {'👍', '❤️', '😂', '😮', '😢'};
 
   static final Uri newIssueUrl = Uri(
     scheme: 'https',
     host: 'github.com',
-    path: '/krille-chan/fluffychat/issues/new',
+    path: '/marcelofmatos/imfxtech-chat/issues/new',
   );
 
   static final Uri homeserverList = Uri(
     scheme: 'https',
     host: 'raw.githubusercontent.com',
-    path: 'krille-chan/fluffychat/refs/heads/main/recommended_homeservers.json',
+    path:
+        'marcelofmatos/imfxtech-chat/refs/heads/main/recommended_homeservers.json',
   );
 
   static final Uri crashReportEndpoint = Uri(
